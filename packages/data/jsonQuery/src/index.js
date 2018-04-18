565271aa251f2050a8783f71726b4106c4dc6194
Peter Mouland <peter.mouland@gmail.com>
2018-04-18 14:18:59 +0100

    wip

--- packages/data/jsonQuery/src/index.js
+++ packages/data/jsonQuery/src/index.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import jsonQuery from 'json-query';
 
-import hart from '../fixtures/player-stats-1001.json';
-// const degea = require('../fixtures/player-stats-1002');
+// import hart from '../fixtures/player-stats-1001.json';
+import dataMap from './data-map';
+import degea from '../fixtures/player-stats-1002.json';
 
-const player = 1001;
-const playerStart = new Date('2018-01-01 16:00:00');
-const playerEnd = new Date('2018-04-01 16:00:00');
+const emptyStats = Array.from(Array(26), () => 0);
 
-const playerFixtures = jsonQuery('fixtures[*status!=PENDING][*:date]', {
-  data: hart,
-  locals: {
-    date(item) {
-      const fixtureDate = new Date(item.date);
-      return fixtureDate <= playerEnd && fixtureDate >= playerStart;
+const summariseStats = (stats) => stats.reduce((prev, curr) => (
+  curr.stats.map((stat, index) => stat + prev[index])
+), emptyStats);
+
+const getStatsWithinTimeFrame = (data, { start, end }) => (
+  jsonQuery('fixtures[*status!=PENDING][*:date]', {
+    data,
+    locals: {
+      date(item) {
+        const fixtureDate = new Date(item.date);
+        return fixtureDate <= end && fixtureDate >= start;
+      },
     },
-  },
-});
+  })
+);
 
-const result = playerFixtures.value.reduce((prev, curr) => {
-  const [c1, c2, c3] = curr.stats;
-  const [c1p, c2p, c3p] = prev;
-  return [c1 + c1p, c2 + c2p, c3 + c3p];
-}, []);
+const addSummaryToStats = ({ data, start, end }) => {
+  const playerFixtures = getStatsWithinTimeFrame(data, { start, end });
+  const statsWithinTeamArray = summariseStats(playerFixtures.value);
+  const statsWithinTeam = dataMap(statsWithinTeamArray);
+  const fixturesWithinTeam = playerFixtures.value;
+  return { ...data, fixturesWithinTeam, statsWithinTeam };
+};
 
+
+
+const start = new Date('2018-01-01 16:00:00');
+const end = new Date('2018-04-01 16:00:00');
+const statsWithSummary = addSummaryToStats({ data: degea, start, end });
 
 const show = () => (
   <pre>
-    {JSON.stringify(playerFixtures.value, null, 2)}
+    {JSON.stringify(statsWithSummary, null, 2)}
   </pre>
 );
 
