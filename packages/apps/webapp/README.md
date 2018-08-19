39403a45750c0a2132d6aefe6c81999b7d65b72d
Peter Mouland <peter.mouland@gmail.com>
2018-08-19 23:39:33 +0100

    nuke

--- packages/apps/webapp/README.md
+++
@@ -1,1 +0,0 @@
-# Kammy User Website
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-dynamic-require, global-require */
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
