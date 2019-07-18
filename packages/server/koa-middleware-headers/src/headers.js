a47b100a4d3e3b292837b3b14be215e1e3d94ae1
Peter Mouland <peter.mouland@gmail.com>
2019-07-18 21:50:29 +0100

    remove content-type css error

--- packages/server/koa-middleware-headers/src/headers.js
+++ packages/server/koa-middleware-headers/src/headers.js
@@ -11,8 +11,6 @@ export default function headers() {
       if (typeof ctx.body === 'string') {
         ctx.set('Content-Length', ctx.body.length);
         ctx.set('etag', crypto.createHash('md5').update(ctx.body).digest('hex'));
-      } else if (typeof ctx.body === 'object') {
-        ctx.set('Content-Type', 'application/json');
       }
     }
   };
